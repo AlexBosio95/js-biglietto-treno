d4680dfc72cec961e754d149ce1189165c4677b2
AlexBosio95 <alex.bosio1995@gmail.com>
2022-06-14 16:40:22 +0200

    calc nel if

--- js/script.js
+++ js/script.js
@@ -9,31 +9,39 @@ const ageUser = parseInt(prompt("Inserisci la tua età"));
 
 // Variabili per calcolo percentuale
 let result = kmUser * 0.21
-let discountUnderage = ((result / 100) * 20)
-let discountOver = ((result / 100) * 40)
-let calcUnderage =  result - discountUnderage
-let calcOver =  result - discountOver
+// let discountUnderage = ((result / 100) * 20)
+// let discountOver = ((result / 100) * 40)
+// let calcUnderage =  result - discountUnderage
+// let calcOver =  result - discountOver
 
 // Calcolo del prezzo secondo l'età
 
 // Minorenni
 if (ageUser < 19) {
-    console.log(`${calcUnderage.toFixed(2)}${" €"}`);
-    document.getElementById("price-user").innerHTML = (`${calcUnderage.toFixed(2)}${" €"}`);
+    // console.log(`${calcUnderage.toFixed(2)}${" €"}`);
+    // document.getElementById("price-user").innerHTML = (`${calcUnderage.toFixed(2)}${" €"}`);
+    console.log((result - ((result / 100) * 20)).toFixed(2) + (" €"));
+    document.getElementById("price-user").innerHTML = ((result - ((result / 100) * 20)).toFixed(2) + (" €"));
+
 } 
 
 // Over
 
 else if (ageUser > 64){
-    console.log(`${calcOver.toFixed(2)}${" €"}`);
-    document.getElementById("price-user").innerHTML = (`${calcOver.toFixed(2)}${" €"}`);
-
+    // console.log(`${calcOver.toFixed(2)}${" €"}`);
+    // document.getElementById("price-user").innerHTML = (`${calcOver.toFixed(2)}${" €"}`);
+    console.log((result - ((result / 100) * 40)).toFixed(2) + (" €"));
+    document.getElementById("price-user").innerHTML = ((result - ((result / 100) * 40)).toFixed(2) + (" €"));
 } 
 
 // Senza sconti
 
 else {
-    console.log(`${result.toFixed(2)}${" €"}`);
-    document.getElementById("price-user").innerHTML = (`${result.toFixed(2)}${" €"}`);
+    // console.log(`${result.toFixed(2)}${" €"}`);
+    // document.getElementById("price-user").innerHTML = (`${result.toFixed(2)}${" €"}`);
+    console.log((result.toFixed(2)) + (" €"));
+    document.getElementById("price-user").innerHTML = ((result.toFixed(2)) + (" €"));
+
+
 }
 
